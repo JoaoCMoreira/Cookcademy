149894c5899f97cb778c03bf5d5eceb13b8c7ce4
JoaoCMoreira <moreira.joaoc@gmail.com>
2023-06-30 16:02:26 +0100

    Create computed property description that returns a String that describes the Ingredient

--- Cookcademy/Recipe.swift
+++ Cookcademy/Recipe.swift
@@ -32,6 +32,23 @@ struct Ingredient {
     var quantity: Double
     var unit: Unit
     
+    var description: String {
+        let formattedQuantity = String(format: "%g", quantity)
+        
+        switch unit {
+        case .none:
+            let formattedName = quantity == 1 ? name: "\(name)s"
+            return "\(formattedQuantity) \(formattedName)"
+        default:
+            if quantity == 1 {
+                return "1 \(unit.singularName) \(name)"
+            } else {
+                return "\(formattedQuantity) \(unit.rawValue) \(name)"
+            
+            }
+        }
+    }
+    
     enum Unit: String, CaseIterable {
         case oz = "Ounces"
         case g = "Grams"
