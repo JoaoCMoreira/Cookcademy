95bc827f2a88bac2dbb4dda2469e986ecd16dd5b
JoaoCMoreira <moreira.joaoc@gmail.com>
2023-06-30 13:55:30 +0100

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by João Moreira on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
