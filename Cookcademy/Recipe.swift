1b3cb061250b92bf3a4a1457f976e8cda15a137d
JoaoCMoreira <moreira.joaoc@gmail.com>
2023-06-30 15:11:28 +0100

    Create Recipe model baseline

--- Cookcademy/Recipe.swift
+++ Cookcademy/Recipe.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+struct Recipe {
+    var mainInformation: MainInformation
+    var ingredients: [Ingredient]
+    var directions: [Direction]
+}
+
+struct MainInformation {
+    var name: String
+    var description: String
+    var author: String
+    var category: Category
+    
+    enum Category: String, CaseIterable {
+        case breakfast = "Breakfast"
+        case lunch = "Lunch"
+        case dinner = "Dinner"
+        case dessert = "Dessert"
+    }
+}
+
+struct Ingredient {
+    var name: String
+    var quantity: Double
+    var unit: Unit
+    
+    enum Unit: String, CaseIterable {
+        case oz = "Ounces"
+        case g = "Grams"
+        case cups = "Cups"
+        case tbs = "Tablespoons"
+        case tsp = "Teaspoons"
+        case none = "No units"
+    }
+}
+
+struct Direction {
+    var description: String
+    var isOptional: Bool
+}
+
+
+
